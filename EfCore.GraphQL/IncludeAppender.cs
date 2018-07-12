acedb449051b5a3ed4822b3dd4709042b39f0ae9
Simon Cropp <simon.cropp@gmail.com>
2018-07-12 14:38:19 +1000

    .

---
+++ EfCore.GraphQL/IncludeAppender.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GraphQL.Language.AST;
+using GraphQL.Types;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreGraphQL
+{
+    public static class IncludeAppender
+    {
+        public static IEnumerable<string> GetPaths<T>(ResolveFieldContext<T> context)
+        {
+            return GetPaths(context.SubFields.Values);
+        }
+
+        public static IQueryable<T> AddIncludes<T>(IQueryable<T> dbSet, ResolveFieldContext<T> context)
+            where T : class
+        {
+            foreach (var path in GetPaths(context))
+            {
+                dbSet = dbSet.Include(path);
+            }
+
+            return dbSet;
+        }
+
+        public static IEnumerable<string> GetPaths(IEnumerable<Field> fields)
+        {
+            var list = new List<string>();
+            foreach (var field in fields)
+            {
+                AddField(list, field, null);
+            }
+
+            return list;
+        }
+
+        static void AddField(List<string> list, Field field, string parentPath)
+        {
+            string path;
+            if (parentPath == null)
+            {
+                path = field.Name;
+            }
+            else
+            {
+                path = $"{parentPath}.{field.Name}";
+            }
+
+            var subFields = field.SelectionSet.Selections.OfType<Field>().ToList();
+            if (!subFields.Any())
+            {
+                return;
+            }
+
+            list.Add(path);
+            foreach (var subField in subFields)
+            {
+                AddField(list, subField, path);
+            }
+        }
+    }
+}
