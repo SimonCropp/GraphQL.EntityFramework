4894143530c8ec5297435a65ac41a6873077954a
Simon Cropp <simon.cropp@gmail.com>
2018-07-17 22:16:04 +1000

    .

--- GraphQL.EntityFramework/ConnectionConverter.cs
+++ GraphQL.EntityFramework/ConnectionConverter.cs
@@ -14,38 +14,73 @@ static class ConnectionConverter
         {
             after = int.Parse(afterString);
         }
+
         int? before = null;
         if (beforeString != null)
         {
             before = int.Parse(beforeString);
         }
+
         return ApplyConnectionContext(list, first, after, last, before);
     }
 
-    public static  Task<Connection<TReturn>> ApplyConnectionContext<TReturn>(IQueryable<TReturn> list, int? first, int? after, int? last, int? before)
+    public static async Task<Connection<TReturn>> ApplyConnectionContext<TReturn>(IQueryable<TReturn> list, int? first, int? after, int? last, int? before)
         where TReturn : class
     {
+        var totalCount = await list.CountAsync().ConfigureAwait(false);
         if (last == null)
         {
-            return First(list, first.GetValueOrDefault(0), after, before);
+            return await First(list, first.GetValueOrDefault(0), after, before, totalCount);
         }
-        return Last(list, last.Value, after, before);
+
+        return await Last(list, last.Value, after, before, totalCount);
     }
 
-    static Task<Connection<TReturn>> First<TReturn>(IQueryable<TReturn> list, int first, int? after, int? before) where TReturn : class
+    static Task<Connection<TReturn>> First<TReturn>(IQueryable<TReturn> list, int first, int? after, int? before, int totalCount) where TReturn : class
     {
         if (before == null)
         {
-            return FirstAfter(list, first, after.GetValueOrDefault(0));
+            return Range(
+                list,
+                skip: after.GetValueOrDefault(0),
+                take: first,
+                totalCount);
         }
-        return FirstBefore(list, first, before.Value);
+
+        return Range(
+            list,
+            skip: Math.Max(before.Value - first, 0),
+            take: first,
+            totalCount);
     }
 
-    static async Task<Connection<TReturn>> FirstAfter<TReturn>(IQueryable<TReturn> list, int first, int after) where TReturn : class
+    static Task<Connection<TReturn>> Last<TReturn>(IQueryable<TReturn> list, int last, int? after, int? before, int count)
+        where TReturn : class
     {
-        var totalCount = await list.CountAsync().ConfigureAwait(false);
-        var take = first;
-        var page = list.Skip(after).Take(take);
+        if (before == null)
+        {
+            // last after
+            return Range(
+                list,
+                skip: after.GetValueOrDefault(-1)+1,
+                take: last,
+                count,
+                true);
+        }
+
+        // last before
+        return Range(
+            list,
+            skip: Math.Max(before.Value - last, 0),
+            take: last,
+            count,
+            true);
+    }
+
+    static async Task<Connection<TReturn>> Range<TReturn>(IQueryable<TReturn> list, int skip, int take, int totalCount, bool reverse=false)
+        where TReturn : class
+    {
+        var page = list.Skip(skip).Take(take);
 
         var result = await page
             .ToListAsync()
@@ -54,46 +89,26 @@ static class ConnectionConverter
             .Select((item, index) =>
                 new Edge<TReturn>
                 {
-                    Cursor = (index + after).ToString(),
+                    Cursor = (index + skip).ToString(),
                     Node = item
                 })
             .ToList();
+        if (reverse)
+        {
+            edges.Reverse();
+        }
+
         return new Connection<TReturn>
         {
             TotalCount = totalCount,
+            Edges = edges,
             PageInfo = new PageInfo
             {
-                HasNextPage = totalCount > take + after,
-                HasPreviousPage = after > 0 && take<totalCount,
-                StartCursor = after.ToString(),
-                EndCursor = Math.Min(totalCount-1, take - 1 + after).ToString(),
-            },
-            Edges = edges
+                HasNextPage = totalCount > take + skip,
+                HasPreviousPage = skip > 0 && take < totalCount,
+                StartCursor = skip.ToString(),
+                EndCursor = Math.Min(totalCount - 1, take - 1 + skip).ToString(),
+            }
         };
-    }
-    static  Task<Connection<TReturn>> FirstBefore<TReturn>(IQueryable<TReturn> list, int first, int before) where TReturn : class
-    {
-        return null;
-    }
-
-    static Task<Connection<TReturn>> Last<TReturn>(IQueryable<TReturn> list, int last, int? after, int? before)
-        where TReturn : class
-    {
-        if (before == null)
-        {
-            return LastAfter(list, last, after.GetValueOrDefault(0));
-        }
-        return LastBefore(list, last, before.Value);
-    }
-
-    static  Task<Connection<TReturn>> LastBefore<TReturn>(IQueryable<TReturn> list, int last, int before)
-        where TReturn : class
-    {
-        return null;
-    }
-    static  Task<Connection<TReturn>> LastAfter<TReturn>(IQueryable<TReturn> list, int last, int after)
-        where TReturn : class
-    {
-        return null;
     }
 }
--- Tests/ConnectionConverterTests.cs
+++ Tests/ConnectionConverterTests.cs
@@ -7,14 +7,29 @@ using Xunit;
 public class ConnectionConverterTests
 {
     [Theory]
-    [InlineData(2, null, null, null)]
-    [InlineData(2, 1, null, null)]
-    [InlineData(3, null, null, null)]
-    [InlineData(3, 1, null, null)]
-    [InlineData(10, null, null, null)]
-    [InlineData(10, 1, null, null)]
-    [InlineData(11, null, null, null)]
-    [InlineData(11, 1, null, null)]
+    //first after
+    //[InlineData(1, 0, null, null)]
+    //[InlineData(2, null, null, null)]
+    //[InlineData(2, 1, null, null)]
+    //[InlineData(3, null, null, null)]
+    //[InlineData(3, 1, null, null)]
+    //[InlineData(10, null, null, null)]
+    //[InlineData(10, 1, null, null)]
+    //[InlineData(11, null, null, null)]
+    //[InlineData(11, 1, null, null)]
+
+    ////first after
+    //[InlineData(2, null, null, 2)]
+    //[InlineData(3, null, null, 2)]
+    //[InlineData(2, null, null, 3)]
+
+    ////last before
+    //[InlineData(null, null, 2, null)]
+    //[InlineData(null, null, 2, 8)]
+
+    //last after
+    [InlineData(null, 7, 2, null)]
+
     public async Task Run(int? first, int? after, int? last, int? before)
     {
         NamerFactory.AdditionalInformation = $"first_{first}_after_{after}_last_{last}_before_{before}";
@@ -22,9 +37,9 @@ public class ConnectionConverterTests
         {
             "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"
         };
-        var connection = await ConnectionConverter.ApplyConnectionContext(new AsyncEnumerable<string>(list), first, after, last, before);
+        var queryable = new AsyncEnumerable<string>(list);
+        var connection = await ConnectionConverter.ApplyConnectionContext(queryable, first, after, last, before);
 
         ObjectApprover.VerifyWithJson(connection);
     }
-
 }
