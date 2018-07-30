ce85020b7a77513f522447bdb72c04d40f248ad2
Simon Cropp <simon.cropp@gmail.com>
2018-07-30 21:21:55 +1000

    doco

--- SampleWeb/Startup.cs
+++ SampleWeb/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using EfCore.InMemoryHelpers;
 using GraphQL.EntityFramework;
 using GraphQL;
 using GraphQL.Types;
@@ -12,44 +11,13 @@ public class Startup
 {
     public void ConfigureServices(IServiceCollection services)
     {
-        var myDataContext = InMemoryContextBuilder.Build<MyDataContext>();
-        var company1 = new Company
-        {
-            Content = "Company1"
-        };
-        var employee1 = new Employee
-        {
-            CompanyId = company1.Id,
-            Content = "Employee1"
-        };
-        var employee2 = new Employee
-        {
-            CompanyId = company1.Id,
-            Content = "Employee2"
-        };
-        var company2 = new Company
-        {
-            Content = "Company2"
-        };
-        var employee4 = new Employee
-        {
-            CompanyId = company2.Id,
-            Content = "Employee4"
-        };
-        var company3 = new Company
-        {
-            Content = "Company3"
-        };
-        var company4 = new Company
-        {
-            Content = "Company4"
-        };
-        myDataContext.AddRange(company1, employee1, employee2, company2,company3,company4, employee4);
-        myDataContext.SaveChanges();
-        services.AddSingleton(myDataContext);
+        services.AddDbContext<MyDataContext>(provider => DataContextBuilder.BuildDataContext());
 
         EfGraphQLConventions.RegisterConnectionTypesInContainer(services);
-        EfGraphQLConventions.RegisterInContainer(services, myDataContext);
+        using (var myDataContext = DataContextBuilder.BuildDataContext())
+        {
+            EfGraphQLConventions.RegisterInContainer(services, myDataContext);
+        }
 
         foreach (var type in GetGraphQlTypes())
         {
@@ -57,12 +25,13 @@ public class Startup
         }
 
         services.AddSingleton<IDocumentExecuter, DocumentExecuter>();
-        services.AddSingleton<IDependencyResolver>(provider => new FuncDependencyResolver(provider.GetRequiredService));
-        services.AddSingleton<ISchema,Schema>();
+        services.AddSingleton<IDependencyResolver>(
+            provider => new FuncDependencyResolver(provider.GetRequiredService));
+        services.AddSingleton<ISchema, Schema>();
         services.AddMvc();
     }
 
-    public static IEnumerable<Type> GetGraphQlTypes()
+    static IEnumerable<Type> GetGraphQlTypes()
     {
         return typeof(Startup).Assembly
             .GetTypes()
