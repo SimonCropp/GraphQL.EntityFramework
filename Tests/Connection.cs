7d9f5089ad491abb2e4e780d8ef13bc0b71a63d4
simoncropp <simon.cropp@gmail.com>
2018-10-18 20:54:50 +1100

    make db unique per runtime

--- Tests/Connection.cs
+++ Tests/Connection.cs
@@ -6,12 +6,17 @@ public static class Connection
 
     static Connection()
     {
+#if(NET472)
+        var dbName = @"GraphQLEntityFrameworkTestsNetClassic";
+#else
+        var dbName = @"GraphQLEntityFrameworkTestsNetCore";
+#endif
         if (Environment.GetEnvironmentVariable("AppVeyor") == "True")
         {
-            ConnectionString = @"Server=(local)\SQL2017;Database=GraphQLEntityFrameworkTests;User ID=sa;Password=Password12!;MultipleActiveResultSets=true";
+            ConnectionString = $@"Server=(local)\SQL2017;Database={dbName};User ID=sa;Password=Password12!;MultipleActiveResultSets=true";
             SqlHelper.EnsureDatabaseExists(ConnectionString);
             return;
         }
-        ConnectionString = @"Data Source=.\SQLExpress;Database=GraphQLEntityFrameworkTests; Integrated Security=True;Max Pool Size=100;MultipleActiveResultSets=true";
+        ConnectionString = $@"Data Source=.\SQLExpress;Database={dbName}; Integrated Security=True;Max Pool Size=100;MultipleActiveResultSets=true";
     }
 }
