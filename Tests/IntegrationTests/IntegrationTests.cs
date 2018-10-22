3dd75bbeb356a9d5868d4ab31c2867d44c108be4
Simon Cropp <simon.cropp@gmail.com>
2018-10-23 10:29:03 +1100

    Update Tests/IntegrationTests/IntegrationTests.cs

--- Tests/IntegrationTests/IntegrationTests.cs
+++ Tests/IntegrationTests/IntegrationTests.cs
@@ -15,14 +15,8 @@ public partial class IntegrationTests : TestBase
 {
     static IntegrationTests()
     {
-        GlobalFilters.Add< FilterParentEntity>((context, item) =>
-        {
-            return item.Property != "Ignore";
-        });
-        GlobalFilters.Add< FilterChildEntity>((context, item) =>
-        {
-            return item.Property != "Ignore";
-        });
+        GlobalFilters.Add<FilterParentEntity>((context, item) => { return item.Property != "Ignore"; });
+        GlobalFilters.Add<FilterChildEntity>((context, item) => { return item.Property != "Ignore"; });
 
         using (var dataContext = BuildDataContext())
         {
