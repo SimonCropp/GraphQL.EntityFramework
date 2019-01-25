933c05f527d3a4c2342f4cc50b4884791ec1d8ee
simoncropp <simon.cropp@gmail.com>
2019-01-25 23:54:47 +1100

    cleanup

--- src/GraphQL.EntityFramework/Where/TypeConverter.cs
+++ src/GraphQL.EntityFramework/Where/TypeConverter.cs
@@ -1,99 +1,114 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
 using GraphQL;
 
 static class TypeConverter
 {
     public static IList ConvertStringsToList(string[] values, Type type)
     {
-        var distinctValues = new HashSet<string>(values);
-
-        var hasNull = distinctValues.Contains(null);
-
-        var newType = type.IsNullable()
-            ? Nullable.GetUnderlyingType(type)
-            : type;
-
-        var valuesExceptNull = distinctValues.Where(s => s != null).ToArray();
-        var result = ConvertStringsToListInternal(valuesExceptNull, newType);
-
-        return type.IsNullable() ? MakeNullableListFrom(result, type, hasNull) : result;
-    }
-
-    private static IList MakeNullableListFrom(IList result, Type newType, bool hasNull = false)
-    {
-        var listOfNullableValues = MakeListInternal(result, newType);
-
-        if (hasNull)
+        if (values.Length != values.Distinct().Count())
         {
-            listOfNullableValues.Add(null);
+            throw new Exception("Duplicates detected for In expression.");
         }
 
-        return listOfNullableValues;
+        var hasNull = values.Contains(null);
+
+        if (!type.IsNullable() && hasNull)
+        {
+            throw new Exception($"Null passed to In expression for non nullable type '{type.FullName}'.");
+        }
+
+        var list = ConvertStringsToListInternal(values.Where(x => x != null), type);
+        if (hasNull)
+        {
+            list.Add(null);
+        }
+        return list;
     }
 
-    private static IList MakeListInternal(IList input, Type type)
-    {
-        var method = typeof(Enumerable).GetMethod(nameof(Enumerable.OfType),BindingFlags.Static | BindingFlags.Public)
-            ?.MakeGenericMethod(type);
-        
-        var args = method.Invoke(null, new object[] {input});
-        
-        var result = (IList) Activator.CreateInstance(typeof(List<>).MakeGenericType(type),args);
-
-        return result;
-    }
-
-    private static IList ConvertStringsToListInternal(string[] values, Type type)
+    static IList ConvertStringsToListInternal(IEnumerable<string> values, Type type)
     {
         if (type == typeof(Guid))
         {
             return values.Select(Guid.Parse).ToList();
+        }
+        if (type == typeof(Guid?))
+        {
+            return values.Select(s => new Guid?(Guid.Parse(s))).ToList();
         }
 
         if (type == typeof(int))
         {
             return values.Select(int.Parse).ToList();
         }
+        if (type == typeof(int?))
+        {
+            return values.Select(s => new int?(int.Parse(s))).ToList();
+        }
 
         if (type == typeof(short))
         {
             return values.Select(short.Parse).ToList();
+        }
+        if (type == typeof(short?))
+        {
+            return values.Select(s => new short?(short.Parse(s))).ToList();
         }
 
         if (type == typeof(long))
         {
             return values.Select(long.Parse).ToList();
         }
+        if (type == typeof(long?))
+        {
+            return values.Select(s => new long?(long.Parse(s))).ToList();
+        }
 
         if (type == typeof(uint))
         {
             return values.Select(uint.Parse).ToList();
+        }
+        if (type == typeof(uint?))
+        {
+            return values.Select(s => new uint?(uint.Parse(s))).ToList();
         }
 
         if (type == typeof(ushort))
         {
             return values.Select(ushort.Parse).ToList();
         }
+        if (type == typeof(ushort?))
+        {
+            return values.Select(s => new ushort?(ushort.Parse(s))).ToList();
+        }
 
         if (type == typeof(ulong))
         {
             return values.Select(ulong.Parse).ToList();
+        }
+        if (type == typeof(ulong?))
+        {
+            return values.Select(s => new ulong?(ulong.Parse(s))).ToList();
         }
 
         if (type == typeof(DateTime))
         {
             return values.Select(DateTime.Parse).ToList();
         }
+        if (type == typeof(DateTime?))
+        {
+            return values.Select(s => new DateTime?(DateTime.Parse(s))).ToList();
+        }
 
         if (type == typeof(DateTimeOffset))
         {
             return values.Select(DateTimeOffset.Parse).ToList();
+        }
+        if (type == typeof(DateTimeOffset?))
+        {
+            return values.Select(s => new DateTimeOffset?(DateTimeOffset.Parse(s))).ToList();
         }
 
         throw new Exception($"Could not convert strings to {type.FullName} ");
