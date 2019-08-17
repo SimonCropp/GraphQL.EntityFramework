ef86b85e347b74950ca7fe2bba1cfc8deb7d1542
Simon Cropp <simon.cropp@gmail.com>
2019-08-17 19:14:53 +1000

    move ArgumentGraphs back to internal

--- src/GraphQL.EntityFramework/Where/Graphs/ArgumentGraphs.cs
+++ src/GraphQL.EntityFramework/Where/Graphs/ArgumentGraphs.cs
@@ -1,37 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using GraphQL.EntityFramework;
 using GraphQL.Types;
 using GraphQL.Utilities;
 
-namespace GraphQL.EntityFramework
+static class ArgumentGraphs
 {
-    public static class ArgumentGraphs
+    static Dictionary<Type, GraphType> entries = new Dictionary<Type, GraphType>();
+
+    static ArgumentGraphs()
     {
-        static Dictionary<Type, GraphType> entries = new Dictionary<Type, GraphType>();
+        GraphTypeTypeRegistry.Register(typeof(Comparison), typeof(ComparisonGraph));
+        GraphTypeTypeRegistry.Register(typeof(StringComparison), typeof(StringComparisonGraph));
+        Add<StringComparisonGraph>();
+        Add<WhereExpressionGraph>();
+        Add<OrderByGraph>();
+        Add<ComparisonGraph>();
+    }
 
-        static ArgumentGraphs()
+    public static void RegisterInContainer(Action<Type, GraphType> registerInstance)
+    {
+        foreach (var entry in entries)
         {
-            GraphTypeTypeRegistry.Register(typeof(Comparison), typeof(ComparisonGraph));
-            GraphTypeTypeRegistry.Register(typeof(StringComparison), typeof(StringComparisonGraph));
-            Add<StringComparisonGraph>();
-            Add<WhereExpressionGraph>();
-            Add<OrderByGraph>();
-            Add<ComparisonGraph>();
+            registerInstance(entry.Key, entry.Value);
         }
+    }
 
-        public static void RegisterInContainer(Action<Type, GraphType> registerInstance)
-        {
-            foreach (var entry in entries)
-            {
-                registerInstance(entry.Key, entry.Value);
-            }
-        }
-
-        static void Add<T>()
-            where T : GraphType, new()
-        {
-            var value = new T();
-            entries.Add(typeof(T), value);
-        }
+    static void Add<T>()
+        where T : GraphType, new()
+    {
+        var value = new T();
+        entries.Add(typeof(T), value);
     }
 }
