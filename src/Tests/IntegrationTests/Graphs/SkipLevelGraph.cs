a8572b12713116fa9cd311cee36c1a8a3094bcdd
Simon Cropp <simon.cropp@gmail.com>
2019-10-05 19:30:17 +1000

    Update SkipLevelGraph.cs

--- src/Tests/IntegrationTests/Graphs/SkipLevelGraph.cs
+++ src/Tests/IntegrationTests/Graphs/SkipLevelGraph.cs
@@ -9,7 +9,7 @@ public class SkipLevelGraph :
         Field(x => x.Id);
         AddNavigationField(
             name: "level3Entity",
-            resolve: context => context.Source.Level2Entity.Level3Entity,
+            resolve: context => context.Source.Level2Entity?.Level3Entity,
             graphType: typeof(Level3Graph),
             includeNames: new[] { "Level2Entity.Level3Entity"});
     }
