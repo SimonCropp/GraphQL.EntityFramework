afb6c54c051642583cf0098421ed264f160bfe39
Simon Cropp <simon.cropp@gmail.com>
2020-07-30 13:42:18 +1000

    .

--- src/Tests/IntegrationTests/IntegrationTests.cs
+++ src/Tests/IntegrationTests/IntegrationTests.cs
@@ -1311,7 +1311,7 @@ fragment inheritedEntityFields on InterfaceGraph {
       items {
         ...childEntityFields
       }
-    }   
+    }
   }
 }
 fragment childEntityFields on DerivedChildGraph {
