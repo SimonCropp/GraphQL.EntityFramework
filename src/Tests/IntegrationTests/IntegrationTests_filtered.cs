1fdc11499b7fea5daa11bd8a7d9eb3bf68042c8a
Simon Cropp <simon.cropp@gmail.com>
2020-08-10 14:39:31 +1000

    Revert "fix connection counts"
    This reverts commit dbd91d6d6c097f6521669a4a00710df1ceccad97.

--- src/Tests/IntegrationTests/IntegrationTests_filtered.cs
+++ src/Tests/IntegrationTests/IntegrationTests_filtered.cs
@@ -44,14 +44,8 @@ public partial class IntegrationTests
     static Filters BuildFilters()
     {
         var filters = new Filters();
-        filters.Add<FilterParentEntity>((context, item) =>
-        {
-            return item.Property != "Ignore";
-        });
-        filters.Add<FilterChildEntity>((context, item) =>
-        {
-            return item.Property != "Ignore";
-        });
+        filters.Add<FilterParentEntity>((context, item) => item.Property != "Ignore");
+        filters.Add<FilterChildEntity>((context, item) => item.Property != "Ignore");
         return filters;
     }
 
