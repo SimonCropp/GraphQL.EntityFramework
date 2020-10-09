f22c08595234f4d7d251b373138355052fa865f6
Simon Cropp <simon.cropp@gmail.com>
2020-10-09 17:33:59 +1100

    Update Navigation.cs

--- src/GraphQL.EntityFramework/Navigation.cs
+++ src/GraphQL.EntityFramework/Navigation.cs
@@ -6,6 +6,8 @@ namespace GraphQL.EntityFramework
     [DebuggerDisplay("Name = {Name}, Type = {Type}")]
     public record Navigation
     (
+        //TODO: guard Name
+        // Guard.AgainstNullWhiteSpace(nameof(name), name);
         string Name,
         Type Type,
         bool IsNullable,
