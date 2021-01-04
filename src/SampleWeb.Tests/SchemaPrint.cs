51f6bca305e400e51b23a5997aa1f0b860af3132
Simon Cropp <simon.cropp@gmail.com>
2021-01-04 19:47:31 +1100

    Update SchemaPrint.cs

--- src/SampleWeb.Tests/SchemaPrint.cs
+++ src/SampleWeb.Tests/SchemaPrint.cs
@@ -4,7 +4,6 @@ using GraphQL.Utilities;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
-using VerifyTests;
 using VerifyXunit;
 using Xunit;
 
