391eb580fe92f442b7308e649c98b1810112b48a
Simon Cropp <simon.cropp@gmail.com>
2022-05-09 11:09:32 +1000

    drop screaming caps from StringComparisonGraph

--- src/GraphQL.EntityFramework/Where/Graphs/StringComparisonGraph.cs
+++ src/GraphQL.EntityFramework/Where/Graphs/StringComparisonGraph.cs
@@ -6,18 +6,11 @@ class StringComparisonGraph :
     public StringComparisonGraph()
     {
         Name = nameof(StringComparison);
-        const string deprecation = "Use the camel case alternative";
         AddValue("currentCulture", null, StringComparison.CurrentCulture);
-        AddValue("CURRENT_CULTURE", null, StringComparison.CurrentCulture, deprecation);
         AddValue("currentCultureIgnoreCase", null, StringComparison.CurrentCultureIgnoreCase);
-        AddValue("CURRENT_CULTURE_IGNORE_CASE", null, StringComparison.CurrentCultureIgnoreCase, deprecation);
         AddValue("invariantCulture", null, StringComparison.InvariantCulture);
-        AddValue("INVARIANT_CULTURE", null, StringComparison.InvariantCulture, deprecation);
         AddValue("invariantCultureIgnoreCase", null, StringComparison.InvariantCultureIgnoreCase);
-        AddValue("INVARIANT_CULTURE_IGNORE_CASE", null, StringComparison.InvariantCultureIgnoreCase, deprecation);
         AddValue("ordinal", null, StringComparison.Ordinal);
-        AddValue("ORDINAL", null, StringComparison.Ordinal, deprecation);
         AddValue("ordinalIgnoreCase", null, StringComparison.OrdinalIgnoreCase);
-        AddValue("ORDINAL_IGNORE_CASE", null, StringComparison.OrdinalIgnoreCase, deprecation);
     }
 }
