28a2f7cab92a7bf7f866c78072e6ab85e60279a0
Simon Cropp <simon.cropp@gmail.com>
2022-08-17 16:34:51 +1000

    Update ArgumentGraphs.cs

--- src/GraphQL.EntityFramework/Where/Graphs/ArgumentGraphs.cs
+++ src/GraphQL.EntityFramework/Where/Graphs/ArgumentGraphs.cs
@@ -4,30 +4,12 @@ using Microsoft.Extensions.DependencyInjection;
 
 static class ArgumentGraphs
 {
-    static Dictionary<Type, GraphType> entries = new();
-
-    [ModuleInitializer]
-    public static void Initialize()
-    {
-        Add<EnumerationGraphType<StringComparison>>();
-        Add<WhereExpressionGraph>();
-        Add<OrderByGraph>();
-        Add<ComparisonGraph>();
-        Add<ConnectorGraph>();
-    }
-
     public static void RegisterInContainer(IServiceCollection services)
     {
-        foreach (var entry in entries)
-        {
-            services.AddSingleton(entry.Key, entry.Value);
-        }
-    }
-
-    static void Add<T>()
-        where T : GraphType, new()
-    {
-        var value = new T();
-        entries.Add(typeof(T), value);
+       services.AddSingleton<EnumerationGraphType<StringComparison>>();
+       services.AddSingleton<WhereExpressionGraph>();
+       services.AddSingleton<OrderByGraph>();
+       services.AddSingleton<ComparisonGraph>();
+       services.AddSingleton<ConnectorGraph>();
     }
 }
