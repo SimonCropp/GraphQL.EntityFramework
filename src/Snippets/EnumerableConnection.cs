63a4c8a3ff10347a6e3782c750c4b0c6ee9e51ff
Simon Cropp <simon.cropp@gmail.com>
2023-12-14 11:36:07 +1100

    Update EnumerableConnection.cs

--- src/Snippets/EnumerableConnection.cs
+++ src/Snippets/EnumerableConnection.cs
@@ -7,14 +7,15 @@ public class EnumerableConnection
     {
         public CompanyGraph()
         {
-            var connectionBuilder = Connection<EmployeeGraph>();
-            connectionBuilder.Name("employees");
-            connectionBuilder.Resolve(context =>
+            var builder = Connection<EmployeeGraph>("employees");
+            builder.Resolve(context =>
             {
                 var skip = context.First.GetValueOrDefault(0);
                 var take = context.PageSize.GetValueOrDefault(10);
                 var list = context.Source.Employees;
-                var page = list.Skip(skip).Take(take);
+                var page = list
+                    .Skip(skip)
+                    .Take(take);
                 return new Connection<Employee>
                 {
                     TotalCount = list.Count,
@@ -23,7 +24,9 @@ public class EnumerableConnection
                         HasNextPage = true,
                         HasPreviousPage = false,
                         StartCursor = skip.ToString(),
-                        EndCursor = Math.Min(list.Count, skip + take).ToString()
+                        EndCursor = Math
+                            .Min(list.Count, skip + take)
+                            .ToString()
                     },
                     Edges = page
                         .Select((item, index) => new Edge<Employee>
