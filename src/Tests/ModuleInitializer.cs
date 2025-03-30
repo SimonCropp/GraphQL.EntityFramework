7afd6dcbdd905672948b1841f6ba14fe4762a4f2
Simon Cropp <simon.cropp@gmail.com>
2025-03-31 10:56:44 +1100

    cleanup

--- src/Tests/ModuleInitializer.cs
+++ src/Tests/ModuleInitializer.cs
@@ -1,6 +1,9 @@
 ﻿public static class ModuleInitializer
 {
     [ModuleInitializer]
-    public static void Init() =>
+    public static void Init()
+    {
         VerifySqlServer.Initialize();
+        VerifierSettings.IgnoreMember("HasTransaction");
+    }
 }
