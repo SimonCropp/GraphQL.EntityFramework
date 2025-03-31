3e5a0878b66af6b741f3f78cc5271d720845a7d6
Simon Cropp <simon.cropp@gmail.com>
2025-03-31 12:39:39 +1100

    Squashed commit of the following:
    commit cff18c4d385a70c65e22dec5cba4cf40cbe932fa
    Author: Simon Cropp <simon.cropp@gmail.com>
    Date:   Mon Mar 31 11:42:53 2025 +1100
    
        .
    
    commit 334839c8165d6cbe09e46f249a7ece0e24185cad
    Author: Simon Cropp <simon.cropp@gmail.com>
    Date:   Mon Mar 31 11:38:26 2025 +1100
    
        make filter work for nav property

--- src/Tests/IntegrationTests/IntegrationTests_filtered.cs
+++ src/Tests/IntegrationTests/IntegrationTests_filtered.cs
@@ -42,6 +42,7 @@
         var filters = new Filters<IntegrationDbContext>();
         filters.Add<FilterParentEntity>((_, _, _, item) => item.Property != "Ignore");
         filters.Add<FilterChildEntity>((_, _, _, item) => item.Property != "Ignore");
+        filters.Add<Level3Entity>((_, _, _, item) => item.Property != "Ignore");
         return filters;
     }
 
@@ -149,5 +150,41 @@
 
         await using var database = await sqlInstance.Build();
         await RunQuery(database, query, null, BuildFilters(), false, [entity1, entity2, entity3]);
+    }
+
+    [Fact]
+    public async Task Multiple_nested_Filtered()
+    {
+        var query =
+            """
+            {
+              level1Entities
+              {
+                level2Entity
+                {
+                  level3Entity
+                  {
+                    property
+                  }
+                }
+              }
+            }
+            """;
+
+        var level3 = new Level3Entity
+        {
+            Property = "Ignore"
+        };
+        var level2 = new Level2Entity
+        {
+            Level3Entity = level3
+        };
+        var level1 = new Level1Entity
+        {
+            Level2Entity = level2
+        };
+
+        await using var database = await sqlInstance.Build();
+        await RunQuery(database, query, null, BuildFilters(), false, [level1, level2, level3]);
     }
 }
