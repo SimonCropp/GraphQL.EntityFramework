f17c3d2fdc636c8e90816529b6a0fe1fb25fc0cd
Simon Cropp <simon.cropp@gmail.com>
2026-02-17 09:23:49 +1100

    Revert "."
    This reverts commit 4a8444bbaad9479156e08479b1f95c4d747ee40d.

--- src/Snippets/GlobalUsings.cs
+++ src/Snippets/GlobalUsings.cs
@@ -1,6 +1,4 @@
-﻿global using System.Security.Claims;
-global using EfLocalDbXunitV3;
-global using GraphQL.EntityFramework;
+﻿global using GraphQL.EntityFramework;
 global using GraphQL.Types.Relay.DataObjects;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata;
