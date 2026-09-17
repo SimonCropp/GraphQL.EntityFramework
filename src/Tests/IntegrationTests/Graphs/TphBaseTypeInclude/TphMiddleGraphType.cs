a3c42e4c330ebc5bea74db77222c89117668ea03
Simon Cropp <simon.cropp@gmail.com>
2026-09-17 15:44:58 +1000

    Update TphMiddleGraphType.cs

--- src/Tests/IntegrationTests/Graphs/TphBaseTypeInclude/TphMiddleGraphType.cs
+++ src/Tests/IntegrationTests/Graphs/TphBaseTypeInclude/TphMiddleGraphType.cs
@@ -2,7 +2,7 @@ public class TphMiddleGraphType :
     EfInterfaceGraphType<IntegrationDbContext, TphMiddleEntity>
 {
     public TphMiddleGraphType(IEfGraphQLService<IntegrationDbContext> graphQlService) :
-        base(graphQlService, _ => _.Attachments) =>
+        base(graphQlService, [_ => _.Attachments]) =>
         AddNavigationListField(
             name: "attachments",
             projection: _ => _.Attachments);
